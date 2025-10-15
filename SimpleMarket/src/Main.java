e3a2627ed31170d8fca2ed44991e3d0456001d1b
a-skrip <alex-himki@mail.ru>
2025-10-15 14:39:41 +0300

    рефакторинг

--- SimpleMarket/src/Main.java
+++ SimpleMarket/src/Main.java
@@ -3,9 +3,7 @@ import handler.MainUserHandler;
 public class Main {
 
     public static void main(String[] args) {
-
         MainUserHandler mainUserHandler = new MainUserHandler();
         mainUserHandler.startHandleInput();
-
     }
 }
